f6ec9f761cd2aee62f27128b34007be6abbb8192
Guilherme Tozzi Mafra <80405986+GuilhermeTozziMafra@users.noreply.github.com>
2024-01-23 16:30:51 -0300

    Update aula2.py

--- Aulas_Python/Aula1-10/aula2.py
+++ Aulas_Python/Aula1-10/aula2.py
@@ -1,1 +1,7 @@
+print(12, 34, 1011, sep='-', end='\n') #sep ='' seria um separador
+print(56, 78, sep='-', end='\n')
+print(9, 10, sep='-', end='\n') 
 
+#CRLF = \r\n para quebrar a linha que é um caracter do windows
+#\n -> LF
+#end='' para finalizar uma linha
